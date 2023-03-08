0a2ff9717d9d97745c0a5a6899e222dbc9f6a00b
Sami Fouad <sfouad@gmail.com>
2023-03-07 22:43:12 -0700

    icon tweak

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,9 @@
 </style>
 
 <div class="grid content-center md:grid-cols-3 lg:grid-cols-5 gap-y-8 gap-x-2.5">
-    <div class="col-span-1 w-40 mx-auto"><a href="/rexrs"><img class="inline text-center" alt="rex.rs" src="/icon-re.png"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/fawldev"><img class="inline text-center" alt="fawl.dev" src="/icon-fd.png"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/kocodev"><img class="inline text-center" alt="koco.dev" src="/icon-ko.png"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/loonieapp"><img class="inline text-center" alt="loonie.app" src="/icon-ln.png"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/fawlapp"><img class="inline text-center" alt="fawl.app" src="/icon-fa.png"/></a></div>
+    <div class="col-span-1 w-40 mx-auto"><a href="/rexrs"><img class="inline text-center" alt="rex.rs" width="124" height="144" src="/card-re.svg"/></a></div>
+    <div class="col-span-1 w-40 mx-auto"><a href="/fawldev"><img class="inline text-center" alt="fawl.dev" width="124" height="144" src="/card-fd.svg"/></a></div>
+    <div class="col-span-1 w-40 mx-auto"><a href="/kocodev"><img class="inline text-center" alt="koco.dev" width="124" height="144" src="/card-ko.svg"/></a></div>
+    <div class="col-span-1 w-40 mx-auto"><a href="/loonieapp"><img class="inline text-center" alt="loonie.app" width="124" height="144" src="/card-ln.svg"/></a></div>
+    <div class="col-span-1 w-40 mx-auto"><a href="/fawlapp"><img class="inline text-center" alt="fawl.app" width="124" height="144" src="/card-fa.svg"/></a></div>
 </div>
