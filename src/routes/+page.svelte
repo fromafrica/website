510a3bee68fd96206d66f12027680f1e417fda50
Sami Fouad <sfouad@gmail.com>
2023-03-08 02:41:13 -0700

    grid layout++

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,112 @@
 </script>
 
 <style>
-
+   @media only screen and (max-width: 480px) {
+        .wrapper {
+          width: 98%;
+          margin: 0 auto;
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-auto-rows: 150px;
+          grid-gap: 5px;
+          grid-auto-flow: dense;
+        }
+        .item {
+          color: white;
+          padding: 0px;
+          border-radius: 4px;
+        }
+        .item:nth-child(even) {
+          /* background: #ececec; */
+        }
+        .hero {
+          grid-column: span 2;
+          grid-row: span 2;
+        }
+        .interesting {
+          grid-column: span 2;
+          grid-row: span 1;
+        }
+        .vert {
+          grid-column: span 1;
+          grid-row: span 2;
+        }
+      }
+      @media only screen and (min-width: 481px) {
+        .wrapper {
+          width: 98%;
+          margin: 0 auto;
+          display: grid;
+          grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
+          grid-auto-rows: 150px;
+          grid-gap: 5px;
+          grid-auto-flow: dense;
+        }
+        .item {
+          color: white;
+          padding: 0px;
+          border-radius: 4px;
+        }
+        .item:nth-child(even) {
+          /* background: #ececec; */
+        }
+        .hero {
+          grid-column: span 2;
+          grid-row: span 2;
+        }
+        .interesting {
+          grid-column: span 2;
+          grid-row: span 1;
+        }
+        .vert {
+          grid-column: span 1;
+          grid-row: span 2;
+        }
+      }
+      @media only screen and (min-width: 630px) {
+        .wrapper {
+          width: 98%;
+          margin: 0 auto;
+          display: grid;
+          grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
+          grid-auto-rows: 150px;
+          grid-gap: 5px;
+          grid-auto-flow: dense;
+        }
+        .item {
+          color: white;
+          padding: 0px;
+          text-align: center;
+          border-radius: 4px;
+        }
+        .item:nth-child(even) {
+          /* background: #ececec; */
+        }
+        .item:hover {
+          cursor: pointer;
+        }
+        .hero {
+          grid-column: span 2;
+          grid-row: span 2;
+        }
+        .interesting {
+          grid-column: span 2;
+          grid-row: span 1;
+        }
+        .vert {
+          grid-column: span 1;
+          grid-row: span 2;
+        }
+      }
 </style>
 
-<div class="grid content-center md:grid-cols-3 lg:grid-cols-5 gap-y-8 gap-x-2.5">
-    <div class="col-span-1 w-40 mx-auto"><a href="/rexrs"><img class="inline text-center" alt="rex.rs" width="124" height="144" src="/card-re.svg"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/fawldev"><img class="inline text-center" alt="fawl.dev" width="124" height="144" src="/card-fd.svg"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/kocodev"><img class="inline text-center" alt="koco.dev" width="124" height="144" src="/card-ko.svg"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/loonieapp"><img class="inline text-center" alt="loonie.app" width="124" height="144" src="/card-ln.svg"/></a></div>
-    <div class="col-span-1 w-40 mx-auto"><a href="/fawlapp"><img class="inline text-center" alt="fawl.app" width="124" height="144" src="/card-fa.svg"/></a></div>
+<div class="wrapper">
+    <div class="item"><img src="card-fa.svg" alt="" style="height: 90%; margin: auto; text-align: center; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <div class="item"><img src="card-re.svg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <div class="item vert"><img src="card-fd.svg" alt="" style="height: 90%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <div class="item"><img src="card-hr.svg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <div class="item"><img src="card-ko.svg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <div class="item"><img src="card-ln.svg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
+    <!-- <div class="item hero bg-stone-900"><img src="love.jpeg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div> -->
+    <div class="item"><img src="card-re.svg" alt="" style="height: 100%; margin: auto; object-fit: cover; overflow: hidden; pointer-events: none; border-radius: 4px;"></div>
 </div>
