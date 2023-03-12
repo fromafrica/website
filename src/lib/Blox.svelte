716d11e6e3d18df077c1581ba34d7d35b3921120
Sami Fouad <sfouad@gmail.com>
2023-03-11 21:01:07 -0700

    version 1 done

--- src/lib/Blox.svelte
+++ src/lib/Blox.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	 export let type: string | null;
 	 export let tag: string | null;
 	 export let name: string | null;
 	 export let footer: string | null;
@@ -13,18 +12,10 @@
 			color: white;
 			background: #f9f9f9;
 			padding: 0px;
+			opacity: 0.9;
+			text-align: center;
 			border-radius: 1px;
-		}
-		.item:nth-child(even) {
-			background: #ececec;
-		}
-		.hero {
-			grid-column: span 2;
-			grid-row: span 2;
-		}
-		.vert {
-			grid-column: span 1;
-			grid-row: span 2;
+  			transition-duration: 0.2s;
 		}
 	}
 	@media only screen and (min-width: 481px) {
@@ -33,18 +24,10 @@
 			color: white;
 			background: #f9f9f9;
 			padding: 0px;
+			opacity: 0.9;
+			text-align: center;
 			border-radius: 1px;
-		}
-		.item:nth-child(even) {
-			background: #ececec;
-		}
-		.hero {
-			grid-column: span 2;
-			grid-row: span 2;
-		}
-		.vert {
-			grid-column: span 1;
-			grid-row: span 2;
+  			transition-duration: 0.2s;
 		}
 	}
 	@media only screen and (min-width: 630px) {
@@ -52,40 +35,24 @@
 			position: relative;
 			color: white;
 			padding: 0px;
-			scale: 1.0;
+			opacity: 0.9;
 			text-align: center;
 			border-radius: 1px;
   			transition-duration: 0.2s;
 		}
-		.item .name {
-			text-shadow: 1px 1px 0px rgba(0,0,0,0.5);
-		}
-		.item:hover {
-			cursor: pointer;
-			scale: 0.99;
-  			transition-duration: 0.2s;
-		}
-		.item:hover .tag {
-			padding-top: 8px;
-			padding-left: 11px;
-  			transition-duration: 0.3s;
-		}
-		.item:hover .footer {
-			padding-bottom: 24px;
-			padding-right: 13px;
-  			transition-duration: 0.3s;
-		}
-		.hero {
-			grid-column: span 2;
-			grid-row: span 2;
-		}
-		.vert {
-			grid-column: span 1;
-			grid-row: span 2;
-		}
 	}
 
-	.tag {
+	.item:nth-child(even) {
+		background: #ececec;
+	}
+
+	.item:hover {
+		cursor: pointer;
+		opacity: 1.0;
+		transition-duration: 0.2s;
+	}
+
+	.item .tag {
 		display: block;
 		width: 100%;
 		text-align: left;
@@ -98,15 +65,16 @@
 		opacity: 0.75;
 	}
 
-	.name {
+	.item .name {
 		display: flex;
 		height: calc(100% - 3rem);
 		font-family: 'Teko', sans-serif;
 		align-items: center;
 		justify-content: center;
+		text-shadow: 1px 1px 0px rgba(0,0,0,0.5);
 	}
 
-	.footer {
+	.item .footer {
 		display: block;
 		position: absolute;
 		bottom: 0;
@@ -123,33 +91,10 @@
 	}
 </style>
 
-
-{#if type === 'small'}
-	<div class="item" style="background: {colour}">
-		{#if tag && name && footer}
-			<div class="tag">{tag}</div>
-			<div class="name" style="font-size: 38px;">{name}</div>
-			<div class="footer"><a href="https://{footer}">{footer}</a></div>
-		{/if}
-	</div>
-{/if}
-
-{#if type === 'vertical'}
-	<div class="item vert" style="background: {colour}">
-		{#if tag && name && footer}
-			<div class="tag">{tag}</div>
-			<div class="name" style="font-size: 52px;">{name}</div>
-			<div class="footer"><a href="https://{footer}">{footer}</a></div>
-		{/if}
-	</div>
-{/if}
-
-{#if type === 'hero'}
-	<div class="item hero" style="background: {colour}">
-		{#if tag && name && footer}
-			<div class="tag">{tag}</div>
-			<div class="name" style="font-size: 96px;">{name}</div>
-			<div class="footer"><a href="https://{footer}">{footer}</a></div>
-		{/if}
-	</div>
-{/if}
+<div class="item" style="background: {colour}">
+	{#if tag && name && footer}
+		<div class="tag">{tag}</div>
+		<div class="name" style="font-size: 38px;">{name}</div>
+		<div class="footer"><a href="https://{footer}">{footer}</a></div>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,56 @@
 <script lang="ts">
 	import Blox from '$lib/Blox.svelte';
+	import BloxVert from '$lib/BloxVert.svelte';
+	import BloxHero from '$lib/BloxHero.svelte';
 </script>
 
 <style>
-	    @media only screen and (max-width: 480px) {
-        .wrapper {
-          width: 98%;
-          margin: 0 auto;
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-          grid-auto-rows: 150px;
-          grid-gap: 4px;
-          grid-auto-flow: dense;
-        }
-      }
-      @media only screen and (min-width: 481px) {
-        .wrapper {
-          width: 98%;
-          margin: 0 auto;
-          display: grid;
-          grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
-          grid-auto-rows: 150px;
-          grid-gap: 4px;
-          grid-auto-flow: dense;
-        }
-      }
-      @media only screen and (min-width: 630px) {
-        .wrapper {
-          width: 98%;
-          margin: 0 auto;
-          display: grid;
-          grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
-          grid-auto-rows: 150px;
-          grid-gap: 4px;
-          grid-auto-flow: dense;
-        }
-      }
+  @media only screen and (max-width: 480px) {
+    .wrapper {
+      width: 98%;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      grid-auto-rows: 150px;
+      grid-gap: 4px;
+      grid-auto-flow: dense;
+    }
+  }
+  @media only screen and (min-width: 481px) {
+    .wrapper {
+      width: 98%;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
+      grid-auto-rows: 150px;
+      grid-gap: 4px;
+      grid-auto-flow: dense;
+    }
+  }
+  @media only screen and (min-width: 630px) {
+    .wrapper {
+      width: 98%;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: repeat( auto-fit, minmax(200px, 3fr) );
+      grid-auto-rows: 150px;
+      grid-gap: 4px;
+      grid-auto-flow: dense;
+    }
+  }
+
+  a {
+    display: contents;
+  }
 </style>
 
 <div class="wrapper">
-    <Blox type="small" tag="117" name="Ak" footer="aksum.dev" colour="#bfa762" />
-    <Blox type="vertical" tag="205" name="Fd" footer="fawl.dev" colour="#d1864b" />
-    <Blox type="small" tag="209" name="Ko" footer="koco.dev" colour="#00a76d" />
-    <Blox type="hero" tag="201" name="Fa" footer="fawl.app" colour="#e93a62" />
-    <Blox type="small" tag="203" name="Ln" footer="loonie.app" colour="#fd4c55" />
-    <Blox type="vertical" tag="105" name="Re" footer="rex.rs" colour="#fbae39" />
-    <Blox type="small" tag="216" name="Lh" footer="linkha.sh" colour="#7d52ea" />
-    <Blox type="small" tag="101" name="Hr" footer="harar.io" colour="#3986FB" />
+  <a href="https://aksum.dev"><Blox tag="117" name="Ak" footer="aksum.dev" colour="#bfa762" /></a>
+  <a href="https://fawl.dev"><BloxVert tag="205" name="Fd" footer="fawl.dev" colour="#d1864b" /></a>
+  <a href="https://koco.dev"><Blox tag="109" name="Ko" footer="koco.dev" colour="#00a76d" /></a>
+  <a href="https://fawl.app"><BloxHero tag="201" name="Fa" footer="fawl.app" colour="#e93a62" /></a>
+  <a href="https://loonie.app"><Blox tag="203" name="Ln" footer="loonie.app" colour="#fd4c55" /></a>
+  <a href="https://rex.rs"><Blox tag="105" name="Re" footer="rex.rs" colour="#fbae39" /></a>
+  <a href="https://linkha.sh"><Blox tag="216" name="Lh" footer="linkha.sh" colour="#7d52ea" /></a>
+  <a href="https://harar.io"><Blox tag="101" name="Hr" footer="harar.io" colour="#3986FB" /></a>
 </div>
