2bad74761d43d045be8cc7064500ec4e585cdd82
Sami Fouad <sfouad@gmail.com>
2023-03-21 10:30:45 -0600

    layout tweaks, mobile testing

---
+++ src/lib/MobileMenu.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
+
+
+<a href="/platform" class="text-2xl">Platform</a><br/><br/>
+<a href="/andromida" class="text-2xl">Andromida</a><br/><br/>
+<a href="/payments" class="text-2xl">Payments</a><br/><br/>
+<br/>
+<a href="/discord">Discord</a><br/>
+<a href="/github">Github</a><br/>
+<a href="/twitter">Twitter</a><br/>
+<a href="/instagram">Instagram</a><br/>
+<a href="/blog">Blog</a><br/>
+<a href="/tutorials">Tutorials</a><br/><br/>
+<br/>
+<a href="/donate" class="text-2xl">Donate</a>
